a397a3bedf327960389e350ddfaf3d391cd1eee0
pandazii <tonytje9@hotmail.com>
2020-04-06 16:45:22 +0200

    commit

--- src/components/Navigation/BottomTabs.js
+++ src/components/Navigation/BottomTabs.js
@@ -49,8 +49,6 @@ export default function BottomTabs() {
                 )
             }} />
         </Tab.Navigator>
-
-
     )
 }
 
--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -3,23 +3,87 @@ import React from 'react'
 import {
     View,
     Text,
-    SafeAreaView
+    SafeAreaView,
+    StyleSheet,
+    ScrollView
 }
     from 'react-native'
 
 export default function HomeScreen() {
     return (
-        <SafeAreaView>
-            <View>
-                <Text>
-                    Test
-            </Text>
-            </View>
+        <SafeAreaView style={HomeScreenStyles.container}>
+            <ScrollView contentContainerStyle={{ flexGrow: 2, height: 'auto' }} horizontal>
+                <View style={HomeScreenStyles.item}>
+                    <Text>
+                        Test
+                </Text>
+                </View>
+                <View style={HomeScreenStyles.item}>
+                    <Text>
+                        Test
+                </Text>
+                </View>
+                <View style={HomeScreenStyles.item}>
+                    <Text>
+                        Test
+                </Text>
+                </View>
+                <View style={HomeScreenStyles.item}>
+                    <Text>
+                        Test
+                </Text>
+                </View>
+                <View style={HomeScreenStyles.item}>
+                    <Text>
+                        Test
+                </Text>
+                </View>
+                <View style={HomeScreenStyles.item}>
+                    <Text>
+                        Test
+                     </Text>
+                </View>
+                <View style={HomeScreenStyles.item}>
+                    <Text>
+                        Test
+                 </Text>
+                </View>
+                <View style={HomeScreenStyles.item}>
+                    <Text>
+                        Test
+                 </Text>
+                </View>
+                <View style={HomeScreenStyles.item}>
+                    <Text>
+                        Test
+                 </Text>
+                </View>
+            </ScrollView>
+            <ScrollView contentContainerStyle={{ flexGrow: 5 }} horizontal>
+                <View style={{ flex: 10, backgroundColor: 'green' }} />
+            </ScrollView>
         </SafeAreaView>
     )
-
-    const HomeScreenStyles = StyleSheet.create({
-
-    })
 }
+
+const HomeScreenStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'grey'
+    },
+
+    item: {
+        paddingVertical: 8,
+        borderWidth: 0,
+        borderColor: "red",
+        borderRadius: 6,
+        backgroundColor: "white",
+        color: "#20232a",
+        textAlign: "center",
+        fontSize: 30,
+        fontWeight: "bold",
+        margin: 15,
+        height: 100
+    }
+})
 
