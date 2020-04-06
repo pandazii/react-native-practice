d454b86109edd9571a69beea037688bdf21f2728
pandazii <tonytje9@hotmail.com>
2020-04-06 13:01:08 +0200

    commit

--- App.js
+++ App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { Ionicons } from 'react-native-vector-icons/Ionicons';
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
+  SafeAreaView,
   View,
-  Text,
+  Text
 } from 'react-native';
 
-import GridScreen from './src/screens/Home/GridScreen'
+import GridScreen from './src/screens/Practice/GridScreen'
+
+const Tab = createBottomTabNavigator();
+
 
 export default function App() {
   return (
-    <GridScreen />
+    <SafeAreaView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={GridScreen} key={1} />
+          <Tab.Screen name="Wedstrijden" component={GridScreen} key={1} />
+          <Tab.Screen name="Spelers" component={GridScreen} key={1} />
+          <Tab.Screen name="Menu" component={GridScreen} key={1} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white'
-  },
-  style: {
-
   }
 });
 
---
+++ src/screens/Practice/GridScreen.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text
+} from 'react-native';
+
+export default function GridScreen() {
+    return (
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+            {/* Row */}
+            <View style={{ flex: 1, flexDirection: 'row', backgroundColor: 'red' }} />
+            {/* Row */}
+            <View style={{ flex: 2, flexDirection: 'row', backgroundColor: 'skyblue' }} />
+            {/* Row */}
+            <View style={{ flex: 2, flexDirection: 'row', backgroundColor: 'steelblue' }}>
+                {/* Col */}
+                <View style={{ flex: 3, flexDirection: 'column', backgroundColor: 'brown' }}>
+                    {/* Row */}
+                    <View style={{ flex: 0, height: 'auto', flexDirection: 'row', backgroundColor: 'blue', justifyContent: 'center' }}>
+                        <Text style={{ color: "white" }}>
+                            Tony</Text>
+                    </View>
+                    {/* Row */}
+                    <View style={{ flex: 0, height: 'auto', flexDirection: 'row', backgroundColor: 'blue', justifyContent: 'center' }}>
+                        <Text style={{ color: "white" }}>
+                            Tony</Text>
+                    </View>
+                    {/* Row */}
+                    <View style={{ flex: 0, height: 'auto', flexDirection: 'row', backgroundColor: 'blue', justifyContent: 'center' }}>
+                        <Text style={{ color: "white" }}>
+                            Tony</Text>
+                    </View>
+                </View>
+                {/* Col */}
+                <View style={{ flex: 2, flexDirection: 'column', backgroundColor: 'red' }}>
+                    {/* Row */}
+                    <View style={{ flex: 0, height: 'auto', flexDirection: 'row', backgroundColor: 'blue', justifyContent: 'center' }}>
+                        <Text style={{ color: "white" }}>
+                            Tony</Text>
+                    </View>
+                    {/* Row */}
+                    <View style={{ flex: 0, height: 'auto', flexDirection: 'row', backgroundColor: 'blue', justifyContent: 'center' }}>
+                        <Text style={{ color: "white" }}>
+                            Tony</Text>
+                    </View>
+                    {/* Row */}
+                    <View style={{ flex: 0, height: 'auto', flexDirection: 'row', backgroundColor: 'blue', justifyContent: 'center' }}>
+                        <Text style={{ color: "white" }}>
+                            Tony</Text>
+                    </View>
+                </View>
+                {/* Col */}
+                <View style={{ flex: 2, flexDirection: 'column', backgroundColor: 'brown' }}>
+                    {/* Row */}
+                    <View style={{ flex: 0, height: 'auto', flexDirection: 'row', backgroundColor: 'blue', justifyContent: 'center' }}>
+                        <Text style={{ color: "white" }}>
+                            Tony</Text>
+                    </View>
+                    {/* Row */}
+                    <View style={{ flex: 0, height: 'auto', flexDirection: 'row', backgroundColor: 'blue', justifyContent: 'center' }}>
+                        <Text style={{ color: "white" }}>
+                            Tony</Text>
+                    </View>
+                    {/* Row */}
+                    <View style={{ flex: 0, height: 'auto', flexDirection: 'row', backgroundColor: 'blue', justifyContent: 'center' }}>
+                        <Text style={{ color: "white" }}>
+                            Tony</Text>
+                    </View>
+                </View>
+            </View>
+        </ScrollView>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white'
+    },
+    style: {
+
+    }
+});
+
